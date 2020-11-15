b0d0f74f6949590d60659fc596ad553751a57348
unknown <arunkumar.mishra2019@vitstudent.ac.in>
2020-11-15 21:03:59 +0530

    1st Commit

---
+++ mergesort_multi.c
@@ -0,0 +1,175 @@
+#define SAMPLE_DATA_LENGTH 10000000
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <time.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+/* Structure for passing arguments to sort functin */
+typedef struct {
+	uint8_t *data;
+	uint32_t low;
+	uint32_t high;
+} sort_args_t;
+
+/* Recursive merge sort */
+void *sort(void *args_v);
+
+/* Combining two arrays into one sorted */
+void merge(uint8_t *data, uint32_t low, uint32_t center, uint32_t high);
+
+/* Checks if value at i is bigger as i+1, therefore checks the success of the sort */
+void verify(uint8_t *data, uint32_t length);
+
+int main(int argc, char const *argv[]) {
+	/* Init random number generator */
+	srand(time(NULL));
+
+	/* Create sample Data */
+	uint8_t *data;
+	uint32_t i;
+	data = (uint8_t*) calloc(SAMPLE_DATA_LENGTH, sizeof(uint8_t));
+	for(i=0; i<SAMPLE_DATA_LENGTH; i++) {
+		data[i] = rand() % 255;
+	}
+
+	/* Print status */
+	printf("Data generated.\nSorting...\n");
+
+	/* Sort in new thread */
+	pthread_t tid;
+	sort_args_t args;
+	args.data = data;
+	args.low = 0;
+	args.high = SAMPLE_DATA_LENGTH;
+	pthread_create(&tid, NULL, sort, (void*) &args);
+
+	/* Join thread */
+  	pthread_join(tid, NULL);
+
+	/* Print status */
+	printf("Data sorted.\n");
+
+	/* Verfiy result */
+	verify(data, SAMPLE_DATA_LENGTH);
+
+	/* Free */
+	free(data);
+}
+
+void *sort(void *args_v) { 
+	sort_args_t args = *(sort_args_t*) args_v;
+
+	if(args.low < args.high) {
+		/* Find center */
+		uint32_t center = (args.low + args.high) / 2;
+
+		/* Create args */
+		sort_args_t a[2];
+
+		a[0].data = args.data;
+		a[0].low = args.low;
+		a[0].high = center;
+
+		a[1].data = args.data;
+		a[1].low = center + 1;
+		a[1].high = args.high;
+
+		/* Only use threads if a considerable amount of elements 
+		   must be sorted */
+		if(args.high - args.low > 2000000) {
+			/* Create a thread and let it execute the
+			   function recursivly */
+			pthread_t tid;
+			if(pthread_create(&tid, NULL, sort, (void*) a) != 0) {
+				printf("ERROR: Thread creation failed!\n");
+				exit(1);
+			}
+
+			printf("Spawned new thread\n");
+
+			/* Use the current thread to execute the other half */
+			sort((void*) (a+1));
+
+			/* Wait for thread to finish */
+			pthread_join(tid, NULL);
+		} 
+
+		/* Not enough elements for parallel computing, do it sequential */
+		else {
+			sort((void*) a);
+			sort((void*) (a+1));
+
+		}
+
+
+		/* Merge them togheter */
+		merge(args.data, args.low, center, args.high);
+	}
+
+	return NULL;
+}
+
+void merge(uint8_t *data, uint32_t low, uint32_t center, uint32_t high) {
+	/* Declare needed variables */
+	uint32_t n0, n1, i, j, k;
+	
+	/* Create buffers */
+	uint8_t *buf0, *buf1;
+
+	/* Calc range for the lower and the upper half */
+	n0 = center - low + 1;
+	n1 = high - center;
+
+	/* Allocate space for buffers in perfect size */
+	buf0 = (uint8_t*) calloc(n0+1, sizeof(data[0]));
+	buf1 = (uint8_t*) calloc(n1+1, sizeof(data[0]));
+
+	/* Copy data into buffers */
+	for(i=0; i<n0; i++) {
+		buf0[i] = data[low + i];
+	}
+  
+	/* Copy data into buffers */
+	for(j=0; j<n1; j++) {
+		buf1[j]= data[center + j + 1];
+	}
+  
+	/* Mark end of buffer */
+	buf0[i] = 255;  
+	buf1[j] = 255;
+ 
+	/* Reset counters */
+	i=0;
+	j=0;
+  
+	/* Merge them by copying always the smaller value back */
+	for(k=low; k<=high; k++) {
+		if(buf0[i] <= buf1[j]) {
+			data[k] = buf0[i++];
+		}
+		else {
+			data[k] = buf1[j++];
+		}
+	}
+
+	/* Free buffers */
+	free(buf0);
+	free(buf1);
+ }
+
+void verify(uint8_t *data, uint32_t length) {
+	/* Iterate over data */
+	uint32_t i;
+	for(i=1; i<length; i++) {
+		/* Print error and cancel id i-1 is bigger than i */
+		if(data[i-1] > data[i]) {
+			printf("ERROR: Verification of result failed: Sort error at index %d\n", i);
+			return;
+		}
+	}
+
+	/* If we get here everything is ok */
+	printf("SUCESS: Result verfied.\n");
+}
---
+++ quick sort single.c
@@ -0,0 +1,100 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/time.h>
+
+#define SIZE 100000000
+
+void verif(int *array);
+char *numToString(int num);
+void swap(int **array, int a, int b);
+void timerConvert(struct timeval start, struct timeval end);
+
+void quicksort(int **array, int start, int end)
+{
+    if(start >= end) return;
+    int left = start, right = end + 1;
+    const int bias = (*array)[start];
+    while(1){
+        do{
+            right--;
+            if(right < 0) break;
+        }while((*array)[right] > bias);
+        do{
+            left++;
+            if(left > SIZE - 1) break;
+        }while((*array)[left] < bias);
+        if(left < right) swap(array, left, right);
+        else break;
+    }
+    swap(array, start, right);
+    if(start >= 0) quicksort(array, start, right - 1);
+    if(end < SIZE) quicksort(array, right + 1, end);
+}
+
+int main()
+{
+    srand(time(NULL));
+    int *tab = malloc(sizeof(int) * SIZE), i;
+    struct timeval start, end;
+    printf("[\033[34m*\033[0m] Generating Data... (%s)\n", numToString(SIZE));
+	gettimeofday(&start, NULL);
+	for(i = 0; i < SIZE; i++) tab[i] = rand();
+	gettimeofday(&end, NULL);
+	printf("[\033[32m+\033[0m] Data generated in ");
+    timerConvert(start, end);
+
+    puts("\n[\033[34m*\033[0m] Starting to sort...");
+    gettimeofday(&start, NULL);
+    quicksort(&tab, 0, SIZE-1);
+    gettimeofday(&end, NULL);
+    printf("[\033[32m+\033[0m] Array sorted in ");
+    timerConvert(start, end);
+    verif(tab);
+
+    return 0;
+}
+
+void swap(int **array, int a, int b)
+{
+    int temp = (*array)[a];
+    (*array)[a] = (*array)[b];
+    (*array)[b] = temp;
+}
+
+void verif(int *array)
+{
+    int i, j;
+    for(i = 1; i < SIZE; i++){
+        if(array[i-1] > array[i]){
+            printf("[-] Problem in the sort tab[%d] = %d, tab[%d] = %d\n", i - 1, array[i - 1], i, array[i]);
+            for(j = i - 3; j < i + 10; j++) // display.
+                printf("[ %d ] = %d\n", j, array[j]);
+            return;
+        }
+    }
+    printf("[+] Array correctly sorted\n\n");
+}
+
+char *numToString(int num)
+{
+	char temp[10] = {'0'}, *string = NULL, i = 0, j = 0, size = 0;
+	sprintf(temp, "%d", num);
+	size = strlen(temp) + ((strlen(temp) - 1) / 3);
+	string = malloc(sizeof(char) * size);
+	for(i = 0, j = 0; i < size; i++){
+		if((size - i) % 4 == 0) string[i] = ' ';
+		else string[i] = temp[j], j++;
+	}
+	string[i] = '\0';
+	return string;
+}
+
+void timerConvert(struct timeval start, struct timeval end)
+{
+    int executionTime = (end.tv_sec - start.tv_sec) * 1000;
+    executionTime += (end.tv_usec - start.tv_usec) / 1000;
+    printf("%d sec %d ms\n", executionTime / 1000, executionTime - ((executionTime / 1000) * 1000));
+}
